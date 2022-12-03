9fe05d2df507a76dd9b8440925578c73f06cf8f2
Hiko <43708222+hikmet-kibar@users.noreply.github.com>
2022-12-04 00:40:30 +0100

    update

--- win/bin/dev.ps1
+++ win/bin/dev.ps1
@@ -3,7 +3,7 @@
 function currentDirectory {Split-Path -leaf -path (Get-Location)}
 
 podman run --rm -it `
-  --hostname pluto `
+  --hostname dev `
   --volume "./:/home/$(currentDirectory)" `
   --workdir "/home/$(currentDirectory)" `
   docker.io/hikib/pluto:1.0.0
