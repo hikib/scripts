857b223962d3842f898abf4643f6798010cb3a9b
hikib <43708222+hikib@users.noreply.github.com>
2023-12-25 13:54:08 +0100

    [fix] symlink to directory is now overwritten instead of nested

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-THISDIR=$(dirname $(readlink -f $0))
-mkdir -p $HOME/.local
-ln -svf $THISDIR/uni $HOME/.local/bin
-sudo chmod +x $HOME/.local/bin/*
+THISDIR=$(dirname "$(readlink -f "$0")")
+mkdir -p "$HOME/.local"
+ln -snvf "$THISDIR/uni" "$HOME/.local/bin"
+sudo chmod +x "$HOME"/.local/bin/*
